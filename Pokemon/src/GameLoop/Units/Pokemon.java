01601981628b54a9d9b41730330bfc72e4b648f9
Sagar <Sagar@Sagar-PC>
2017-02-18 19:03:47 +0530

    Pokemon and attack class created

---
+++ Pokemon/src/GameLoop/Units/Pokemon.java
@@ -0,0 +1,17 @@
+
+package GameLoop.Units;
+
+public class Pokemon {
+    enum Type {
+        Grass, Fire, Electric, Water
+    }
+    String name;
+    int id;
+    Type myType;
+    Type weakness;
+    int currHP, maxHP;
+    int exp;
+    int level;
+    
+    
+}
---
+++ Pokemon/src/Utilities/Attack.java
@@ -0,0 +1,14 @@
+package Utilities;
+
+public class Attack {
+    enum Type {
+        Grass, Fire, Electric, Water
+    }
+    String name;
+    Type pokemonType;
+    Type type;
+    int dmg;
+    int currCount;
+    int maxCount;
+    
+}
